2783ce1d858a4191f5a40a23f7bdf6d809e33dd6
harish96880 <sriharishr105@gmail.com>
2023-07-29 14:04:55 +0530

    Converting entire jsx formatted react application into tsx format

---
+++ hello-react/src/TaskCard.tsx
@@ -0,0 +1,17 @@
+import "./TaskCard.css";
+import React from "react";
+
+let TaskCard = (props) => {
+  console.log(props);
+  return (
+    <div className="TaskItem">
+      <h2 className="text-xl font-bold">{props.title}</h2>
+      <p>
+        {props.status}: {props.dueDate}
+      </p>
+      <p>Assignee Name: {props.asigneeName}</p>
+    </div>
+  );
+};
+
+export default TaskCard;
