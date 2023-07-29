422486ddccefd633e34bed72bc8afd710973493c
harish96880 <sriharishr105@gmail.com>
2023-07-29 11:56:56 +0530

    Adding UI

---
+++ hello-react/src/TaskBar.jsx
@@ -0,0 +1,14 @@
+import './TaskCard.css'
+
+let TaskBar = (props) => {
+    console.log(props);
+    return (
+        <div className='TaskItem'>
+            <h2 className='text-xl font-bold'>{props.title}</h2>
+            <p>{props.status}: {props.dueDate}</p>
+            <p>Assignee Name: {props.asigneeName}</p>
+        </div>
+    )
+}
+
+export default TaskBar
