302d955dc718c50431ac65d8c1eef9f72876aa6b
Harish <sriharishr105@gmail.com>
2023-10-02 10:49:16 +0530

    Changes made for  level 8 final commit

--- smarter-tasks/src/pages/Notfound.tsx
+++ smarter-tasks/src/pages/Notfound.tsx
@@ -6,7 +6,7 @@ export const NotFound: React.FC = () => {
       <div className="flex h-screen justify-center items-center">
         <div className="flex flex-col items-center">
           <h1 className="text-8xl  font-bold text-gray-800">404</h1>
-          <p className="text-xl">Pag Not Found</p>
+          <p className="text-xl">404! Page Not Found</p>
         </div>
       </div>
     </>
